b937124e20c83396104045bd88226ccd3ac6a01f
Angelo Pirola <angelo9871@gmail.com>
2022-08-14 20:32:50 +0200

    Primo commit GestioneSagre.Web.Migrations

---
+++ src/GestioneSagre.Web.Migrations/Migrations/20220814164051_AddTabellaCategoria.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestioneSagre.Web.Migrations.Migrations
+{
+    public partial class AddTabellaCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GuidFesta = table.Column<string>(type: "TEXT", nullable: true),
+                    CategoriaVideo = table.Column<string>(type: "TEXT", nullable: true),
+                    CategoriaStampa = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
