9c2705bf4cea01e35fac3afb9750db633fa77bfc
Angelo Pirola <angelo9871@gmail.com>
2022-08-15 16:03:02 +0200

    Aggiunto progetto GestioneSagre.ViewExtensions

---
+++ src/Extensions/GestioneSagre.ViewExtensions/VersioneExtension.cs
@@ -0,0 +1,18 @@
+﻿using GestioneSagre.Core.Models.Entities;
+using GestioneSagre.Models.ViewModels;
+
+namespace GestioneSagre.ViewExtensions;
+
+public static class VersioneExtension
+{
+    public static VersioneViewModel ToVersioneViewModel(this VersioneEntity dataRow)
+    {
+        return new VersioneViewModel
+        {
+            Id = dataRow.Id,
+            CodiceVersione = dataRow.CodiceVersione,
+            TestoVersione = dataRow.TestoVersione,
+            VersioneStato = dataRow.VersioneStato
+        };
+    }
+}
