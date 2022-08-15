3acf48b5025323465650188126aa075bd72a8a87
Angelo Pirola <angelo9871@gmail.com>
2022-08-15 18:52:45 +0200

    Aggiunto Internal Controller

---
+++ src/GestioneSagre.Web.Server/Controllers/InternalController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace GestioneSagre.Web.Server.Controllers;
+
+public class InternalController : BaseController
+{
+    private readonly IInternalService internalService;
+
+    public InternalController(IInternalService internalService)
+    {
+        this.internalService = internalService;
+    }
+
+    [AllowAnonymous]
+    [HttpGet("GeneraGuid")]
+    public Task<string> GenerateGuid()
+    {
+        return internalService.GenerateGuid();
+    }
+
+    [AllowAnonymous]
+    [HttpGet("GeneraImmagineDefault")]
+    public Task<string> GenerateImageDefault()
+    {
+        return internalService.GenerateImageDefault();
+    }
+}
