44859b8b868eeffcc6cb1b125874202eb1f14587
Angelo Pirola <angelo9871@gmail.com>
2022-08-18 00:17:57 +0200

    Refactoring GestioneSagre.Categorie.QueryStack

---
+++ src/QueryStack/GestioneSagre.Categorie.QueryStack/ICategoriaQueryStackService.cs
@@ -0,0 +1,12 @@
+﻿using GestioneSagre.Models.InputModels.Categoria;
+
+namespace GestioneSagre.Categorie.QueryStack;
+
+public interface ICategoriaQueryStackService
+{
+    Task<List<CategoriaViewModel>> GetCategorieAsync();
+    Task<CategoriaViewModel> GetCategoriaAsync(CategoriaDetailViewModel viewModel);
+    Task<CategoriaEditInputModel> GetCategoriaForEditingAsync(CategoriaDetailViewModel viewModel);
+    Task<bool> IsCategoriaAvailableAsync(CategoriaDetailViewModel viewModel);
+    Task<int> GetCountProdottiByCategoriaAsync(CategoriaDetailViewModel viewModel);
+}
