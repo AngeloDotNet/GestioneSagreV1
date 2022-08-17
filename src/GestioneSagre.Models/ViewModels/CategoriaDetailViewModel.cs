41738a827db1bcd62ff97bf08b7b039af6721680
Angelo Pirola <angelo9871@gmail.com>
2022-08-18 00:51:50 +0200

    Refactoring GestioneSagre.Models

--- src/GestioneSagre.Models/ViewModels/CategoriaDetailViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GestioneSagre.Models.ViewModels;
-
-public class CategoriaDetailViewModel
-{
-    public int Id { get; set; }
-    public string GuidFesta { get; set; }
-    public string CategoriaVideo { get; set; }
-}
