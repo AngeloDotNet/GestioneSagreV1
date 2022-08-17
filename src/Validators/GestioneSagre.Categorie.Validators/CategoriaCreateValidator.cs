21dc22093e5f7e513917fdcb204c6152eeb791c8
Angelo Pirola <angelo9871@gmail.com>
2022-08-18 00:19:16 +0200

    Refactoring GestioneSagre.Categorie.Validators

---
+++ src/Validators/GestioneSagre.Categorie.Validators/CategoriaCreateValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace GestioneSagre.Categorie.Validators;
+
+public class CategoriaCreateValidator : AbstractValidator<CategoriaCreateInputModel>
+{
+    public CategoriaCreateValidator()
+    {
+        RuleFor(x => x.GuidFesta)
+            .NotEmpty().WithMessage("Il guid della festa è obbligatorio");
+
+        RuleFor(x => x.CategoriaVideo)
+            .NotEmpty().WithMessage("La categoria video è obbligatoria");
+
+        RuleFor(x => x.CategoriaStampa)
+            .NotEmpty().WithMessage("La categoria stampa è obbligatoria");
+    }
+}
