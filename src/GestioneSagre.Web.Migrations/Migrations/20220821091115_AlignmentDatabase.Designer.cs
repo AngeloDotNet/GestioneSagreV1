275c07fec0c1a14d131a2cd77551a09c0991dadb
Angelo Pirola <angelo9871@gmail.com>
2022-08-21 11:19:26 +0200

    Refactoring DBContext e Database SQLite

--- src/GestioneSagre.Web.Migrations/Migrations/20220821091115_AlignmentDatabase.Designer.cs
+++ src/GestioneSagre.Web.Migrations/Migrations/20220821091115_AlignmentDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using GestioneSagre.Domain.Services.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestioneSagre.Web.Migrations.Migrations
 {
     [DbContext(typeof(GestioneSagreDbContext))]
-    partial class GestioneSagreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220821091115_AlignmentDatabase")]
+    partial class AlignmentDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
