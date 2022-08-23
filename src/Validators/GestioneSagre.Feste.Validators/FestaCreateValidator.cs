96e2b04fc5a7ae9ffffa39216e18a106aff7694f
Angelo Pirola <angelo9871@gmail.com>
2022-08-23 12:50:19 +0200

    Aggiunto progetto GestioneSagre.Feste.Validators

---
+++ src/Validators/GestioneSagre.Feste.Validators/FestaCreateValidator.cs
@@ -0,0 +1,49 @@
+﻿namespace GestioneSagre.Feste.Validators;
+
+public class FestaCreateValidator : AbstractValidator<FestaCreateInputModel>
+{
+    public FestaCreateValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("L'ID è obbligatorio");
+
+        RuleFor(x => x.GuidFesta)
+            .NotEmpty().WithMessage("Il guid della festa è obbligatorio");
+
+        RuleFor(x => x.DataInizio)
+            .NotEmpty().WithMessage("La data di inizio della festa è obbligatoria");
+
+        RuleFor(x => x.DataFine)
+            .NotEmpty().WithMessage("La data di fine della festa è obbligatoria");
+
+        RuleFor(x => x.Titolo)
+            .NotEmpty().WithMessage("Il titolo della festa è obbligatorio");
+
+        RuleFor(x => x.Edizione)
+            .NotEmpty().WithMessage("L'edizione della festa è obbligatorio");
+
+        RuleFor(x => x.Luogo)
+            .NotEmpty().WithMessage("Il luogo della festa è obbligatorio");
+
+        //RuleFor(x => x.Logo)
+        //    .NotEmpty().WithMessage("");
+
+        //RuleFor(x => x.GestioneCoperti)
+        //    .NotEmpty().WithMessage("");
+
+        //RuleFor(x => x.GestioneMenu)
+        //    .NotEmpty().WithMessage("");
+
+        //RuleFor(x => x.StampaCarta)
+        //    .NotEmpty().WithMessage("");
+
+        //RuleFor(x => x.StampaLogo)
+        //    .NotEmpty().WithMessage("");
+
+        //RuleFor(x => x.StampaRicevuta)
+        //    .NotEmpty().WithMessage("");
+
+        //RuleFor(x => x.StatusFesta)
+        //    .NotEmpty().WithMessage("");
+    }
+}
---
+++ src/Validators/GestioneSagre.Feste.Validators/FestaDeleteValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace GestioneSagre.Feste.Validators;
+
+public class FestaDeleteValidator : AbstractValidator<FestaDeleteInputModel>
+{
+    public FestaDeleteValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("L'ID è obbligatorio");
+
+        RuleFor(x => x.GuidFesta)
+            .NotEmpty().WithMessage("Il guid della caegoria è obbligatorio");
+    }
+}
