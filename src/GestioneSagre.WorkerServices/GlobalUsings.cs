b155bba38d7183d713c26e49e7ac9bd97d751906
Angelo Pirola <angelo9871@gmail.com>
2022-08-24 13:57:34 +0200

    Aggiunto nuovo progetto GestioneSagre.WorkerServices

---
+++ src/GestioneSagre.WorkerServices/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using GestioneSagre.Feste.CommandStack;
+global using GestioneSagre.Feste.QueryStack;
+global using GestioneSagre.Models.ViewModels;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
