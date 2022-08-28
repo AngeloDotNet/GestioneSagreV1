8b692aa2ba61bef7d9edf8746c98e0d9430cb971
Angelo Pirola <angelo9871@gmail.com>
2022-08-28 20:56:08 +0200

    Aggiunto progetto GestioneSagre.Prodotti.Validators

---
+++ src/Validators/GestioneSagre.Prodotti.Validators/ProdottoCreateValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace GestioneSagre.Prodotti.Validators;
+
+public class ProdottoCreateValidator : AbstractValidator<ProdottoCreateInputModel>
+{
+    public ProdottoCreateValidator()
+    {
+        RuleFor(x => x.GuidFesta)
+            .NotEmpty().WithMessage("Il guid della categoria è obbligatorio");
+
+        //RuleFor(x => x.CategoriaId)
+        //    .NotEmpty().WithMessage("L'Id della categoria è obbligatorio");
+
+        RuleFor(x => x.Prodotto)
+            .NotEmpty().WithMessage("Il prodotto è obbligatorio");
+
+        RuleFor(x => x.Prezzo)
+            .NotEmpty().WithMessage("Il prezzo è obbligatorio");
+
+        RuleFor(x => x.Quantita)
+            .NotEmpty().WithMessage("La quantità è obbligatoria");
+    }
+}
