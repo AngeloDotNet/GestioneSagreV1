cd78803a6d382cfc4432c9bc576f173d6abb56a0
Angelo Pirola <angelo9871@gmail.com>
2022-08-28 21:54:57 +0200

    Aggiunto progetto GestioneSagre.Internal.Extensions

---
+++ src/Extensions/GestioneSagre.Internal.Extensions/RegisterInternalServices.cs
@@ -0,0 +1,18 @@
+﻿namespace GestioneSagre.Internal.Extensions;
+
+public static class RegisterInternalServices
+{
+    public static IServiceCollection AddRegisterInternalServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Services TRANSIENT - GestioneSagre.XXX.CommandStack
+        services.AddTransient<IVersioneCommandStackService, VersioneCommandStackService>();
+
+        // Services TRANSIENT - GestioneSagre.XXX.QueryStack
+        services.AddTransient<IVersioneQueryStackService, VersioneQueryStackService>();
+
+        // Services SINGLETON
+        services.AddSingleton<IInternalQueryStackService, InternalQueryStackService>();
+
+        return services;
+    }
+}
---
+++ src/Extensions/GestioneSagre.Internal.Extensions/ValidationInternalServices.cs
@@ -0,0 +1,20 @@
+﻿namespace GestioneSagre.Internal.Extensions;
+
+public static class ValidationInternalServices
+{
+    public static IServiceCollection AddValidationInternalServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddFluentValidationAutoValidation(options =>
+        {
+            options.DisableDataAnnotationsValidation = true;
+        });
+
+        // GestioneSagre.Versioni.Validators
+        services.AddValidatorsFromAssemblyContaining<VersioneCreateValidator>();
+
+        // GestioneSagre.Internal.Validators
+        services.AddValidatorsFromAssemblyContaining<MailSupportoSenderValidator>();
+
+        return services;
+    }
+}
