1308a581c17192babc1fa11e2be3084e47d470e7
Angelo Pirola <angelo9871@gmail.com>
2022-08-29 17:07:17 +0200

    Aggiunto progetto GestioneSagre.Web.Shared

---
+++ src/GestioneSagre.Web.Shared/Services/Versione/IVersioneService.cs
@@ -0,0 +1,8 @@
+﻿using GestioneSagre.Web.Shared.Models.ViewModels;
+
+namespace GestioneSagre.Web.Shared.Services.Versione;
+
+public interface IVersioneService
+{
+    Task<VersioneViewModel> GetVersione();
+}
