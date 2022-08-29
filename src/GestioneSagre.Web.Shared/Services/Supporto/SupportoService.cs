7783aad62e341097f837ca53afa8e6ec63ee021d
Angelo Pirola <angelo9871@gmail.com>
2022-08-30 00:12:39 +0200

    Aggiunti nuovi metodi e classi al progetto GestioneSagre.Web.Shared

---
+++ src/GestioneSagre.Web.Shared/Services/Supporto/SupportoService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+using GestioneSagre.Web.Shared.Models.InputSender;
+using GestioneSagre.Web.Shared.Services.Configurazione;
+
+namespace GestioneSagre.Web.Shared.Services.Supporto;
+
+public class SupportoService : ISupportoService
+{
+    private readonly IConfigurazioneService configurazioneService;
+    private HttpClient httpClient;
+
+    public SupportoService(HttpClient httpClient, IConfigurazioneService configurazioneService)
+    {
+        this.httpClient = httpClient;
+        this.configurazioneService = configurazioneService;
+    }
+
+    public async Task InvioEmailSupportoAsync(MailSupportoInputSender inputModel)
+    {
+        var pathWebInternalAPI = await configurazioneService.GetPathInternalAPI();
+
+        var response = await httpClient.PostAsJsonAsync($"https://{pathWebInternalAPI}/api/Email/InvioEmailSupporto", inputModel);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Richiesta di supporto non inviata a causa di un problema tecnico.");
+        }
+    }
+}
