11f3d2e384cba084a3036b67fc78a0bd9fcaffd3
Angelo Pirola <angelo9871@gmail.com>
2022-08-29 17:05:25 +0200

    Aggiunto progetto GestioneSagre.Web.Client

---
+++ src/GestioneSagre.Web.Client/Program.cs
@@ -0,0 +1,42 @@
+using GestioneSagre.Web.Shared.Services.Configurazione;
+using GestioneSagre.Web.Shared.Services.Versione;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace GestioneSagre.Web.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            builder.Services.AddTransient<IConfigurazioneService, ConfigurazioneService>();
+            builder.Services.AddTransient<IVersioneService, VersioneService>();
+
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = false;
+                config.SnackbarConfiguration.VisibleStateDuration = 3000; //5000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
