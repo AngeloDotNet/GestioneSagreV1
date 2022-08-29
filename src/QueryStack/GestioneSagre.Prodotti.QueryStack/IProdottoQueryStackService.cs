4d49ff2009512e144c9eabc1e608094605cc7fb3
Angelo Pirola <angelo9871@gmail.com>
2022-08-29 11:01:16 +0200

    Aggiunto progetto GestioneSagre.Prodotti.QueryStack alla soluzione

---
+++ src/QueryStack/GestioneSagre.Prodotti.QueryStack/IProdottoQueryStackService.cs
@@ -0,0 +1,9 @@
+﻿namespace GestioneSagre.Prodotti.QueryStack;
+
+public interface IProdottoQueryStackService
+{
+    Task<List<ProdottoViewModel>> GetProdottiAsync();
+    Task<ProdottoViewModel> GetProdottoAsync(string guidFesta, string prodotto);
+    Task<bool> IsProdottoAvailableAsync(string guidFesta, string prodotto, int id);
+    Task<int> GetCountProdottiByCategoriaAsync(string guidFesta, int categoriaId);
+}
