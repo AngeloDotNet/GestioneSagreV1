95c86249d5e442fd5d25c6db632fa921f3a7dedf
Angelo Pirola <angelo9871@gmail.com>
2022-08-29 11:21:01 +0200

    Aggiunto progetto GestioneSagre.Prodotti.CommandStack alla soluzione

---
+++ src/CommandStack/GestioneSagre.Prodotti.CommandStack/IProdottoCommandStackService.cs
@@ -0,0 +1,8 @@
+﻿namespace GestioneSagre.Prodotti.CommandStack;
+
+public interface IProdottoCommandStackService
+{
+    Task<ProdottoViewModel> CreateProdottoAsync(ProdottoCreateInputModel inputModel);
+    Task<ProdottoViewModel> EditProdottoAsync(ProdottoEditInputModel inputModel);
+    Task DeleteProdottoAsync(ProdottoDeleteInputModel inputModel);
+}
