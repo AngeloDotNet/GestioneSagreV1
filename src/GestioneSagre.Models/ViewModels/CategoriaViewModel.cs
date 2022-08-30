66393fba08db13a1c525de4a923b102afbb38b00
Angelo Pirola <angelo9871@gmail.com>
2022-08-30 13:49:11 +0200

    Refactoring

--- src/GestioneSagre.Models/ViewModels/CategoriaViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestioneSagre.Models.ViewModels;
-
-public class CategoriaViewModel
-{
-    public int Id { get; set; }
-    public string GuidFesta { get; set; }
-    public string CategoriaVideo { get; set; }
-    public string CategoriaStampa { get; set; }
-}
--- src/GestioneSagre.Models/ViewModels/VersioneViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestioneSagre.Models.ViewModels;
-
-public class VersioneViewModel
-{
-    public int Id { get; set; }
-    public string CodiceVersione { get; set; }
-    public string TestoVersione { get; set; }
-    public VersioneStato VersioneStato { get; set; }
-}
