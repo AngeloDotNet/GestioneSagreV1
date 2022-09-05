bcc7fde9b2b89172dc75e52aa39af26eb56c3a0a
Angelo Pirola <angelo9871@gmail.com>
2022-09-05 22:17:05 +0200

    Creata cartella ClientApp ed aggiunti relativi progetti

---
+++ src/ClientApp/GestioneSagre.Web.Shared/Services/Configurazione/IConfigurazioneService.cs
@@ -0,0 +1,8 @@
+﻿namespace GestioneSagre.Web.Shared.Services.Configurazione;
+
+public interface IConfigurazioneService
+{
+    Task<string> GetApplicationApiFromSettings();
+    Task<string> GetInternalApiFromSettings();
+    Task<string> GetVersioneFromSettings();
+}
