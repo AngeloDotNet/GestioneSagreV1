af905589adc8981956835cfc4cc0b3d3a948215a
Angelo Pirola <angelo9871@gmail.com>
2022-09-08 16:19:39 +0200

    Aggiunto progetto GestioneSagre.Web.ExternalApi

---
+++ src/GestioneSagre.Web.ExternalApi/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.Reflection;
+global using System.Text.Json.Serialization;
+global using GestioneSagre.Core.Models.Options;
+global using GestioneSagre.Domain.Services.Infrastructure;
+global using Microsoft.AspNetCore.Server.Kestrel.Core;
+global using Microsoft.EntityFrameworkCore;
---
+++ src/GestioneSagre.Web.ExternalApi/Startup.cs
@@ -0,0 +1,83 @@
+﻿namespace GestioneSagre.Web.ExternalApi;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+            });
+
+        services.AddValidationExternalServices(Configuration);
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            });
+        });
+
+        services.AddDbContextPool<GestioneSagreInternalDbContext>(optionBuilder =>
+        {
+            var connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
+
+            optionBuilder.UseSqlite(connectionString, options =>
+            {
+                options.MigrationsAssembly("GestioneSagre.Web.ExternalApi");
+            });
+        });
+
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+        // Customized services
+        services.AddSwaggerExternalServices(Configuration, xmlPath);
+        services.AddRegisterExternalServices(Configuration);
+
+        // Options
+        services.Configure<KestrelServerOptions>(Configuration.GetSection("Kestrel"));
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        var enableSwagger = Configuration.GetSection("Swagger").GetValue<bool>("enabled");
+
+        if (enableSwagger)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre v1");
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseCors();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
