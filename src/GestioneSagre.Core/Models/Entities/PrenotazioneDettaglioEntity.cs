fe6bf777441bcafd54dc3c24679a2495d74f2900
Angelo Pirola <angelo9871@gmail.com>
2022-09-08 15:14:36 +0200

    Create entità prenotazione e prenotazione dettaglio

---
+++ src/GestioneSagre.Core/Models/Entities/PrenotazioneDettaglioEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace GestioneSagre.Core.Models.Entities;
+
+public class PrenotazioneDettaglioEntity
+{
+    public int Id { get; set; }
+    public int PrenotazioneId { get; set; }
+    public string Prodotto { get; set; }
+    public int Quantita { get; set; }
+    public Money Prezzo { get; set; }
+    public Money Totale { get; set; }
+}
---
+++ src/GestioneSagre.Core/Models/Entities/PrenotazioneEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace GestioneSagre.Core.Models.Entities;
+
+public class PrenotazioneEntity
+{
+    public int Id { get; set; }
+    public string GuidFesta { get; set; }
+    public string Riferimento { get; set; }
+    public string Nominativo { get; set; }
+    public string DataPrenotazione { get; set; }
+    public string OraPrenotazione { get; set; }
+    public PrenotazioneStato StatoPrenotazione { get; set; }
+}
