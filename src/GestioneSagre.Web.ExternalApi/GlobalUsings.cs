05f4aaeb10e710b14a08b65695a17c396d74a5bf
Angelo Pirola <angelo9871@gmail.com>
2022-09-09 11:46:23 +0200

    Gestiti usings a livello globale di progetto

--- src/GestioneSagre.Web.ExternalApi/GlobalUsings.cs
+++ src/GestioneSagre.Web.ExternalApi/GlobalUsings.cs
@@ -1,6 +1,6 @@
 ﻿global using System.Reflection;
 global using System.Text.Json.Serialization;
-global using GestioneSagre.Core.Models.Options;
 global using GestioneSagre.Domain.Services.Infrastructure;
+global using GestioneSagre.External.Extensions;
 global using Microsoft.AspNetCore.Server.Kestrel.Core;
 global using Microsoft.EntityFrameworkCore;
