a94e8b1a36ad203d6b49e4e323f683082399b1e5
Angelo Pirola <angelo9871@gmail.com>
2022-09-20 16:11:48 +0200

    Aggiunto database progetto GestioneSagre.Web.SecurityApi e relativo DbContext

---
+++ src/GestioneSagre.Domain/Services/Infrastructure/GestioneSagreSecurityDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace GestioneSagre.Domain.Services.Infrastructure;
+
+public partial class GestioneSagreSecurityDbContext : DbContext
+{
+    public GestioneSagreSecurityDbContext(DbContextOptions<GestioneSagreSecurityDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
