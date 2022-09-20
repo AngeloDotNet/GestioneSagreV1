9524cc8fc92afcf933a136e0985064166838eead
Angelo Pirola <angelo9871@gmail.com>
2022-09-20 15:51:36 +0200

    Refactoring post creazione progetto

--- src/GestioneSagre.Web.SecurityApi/Startup.cs
+++ src/GestioneSagre.Web.SecurityApi/Startup.cs
@@ -1,59 +1,71 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+namespace GestioneSagre.Web.SecurityApi;
 
-namespace GestioneSagre.Web.SecurityApi
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddControllers()
+             .AddJsonOptions(options =>
+             {
+                 options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+             });
+
+        services.AddCors(options =>
         {
-            Configuration = configuration;
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            });
+        });
+
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+        // Customized services
+        //services.AddSwaggerSecurityServices(Configuration, xmlPath);
+        //services.AddRegisterSecurityServices(Configuration);
+
+        // Options
+        services.Configure<KestrelServerOptions>(Configuration.GetSection("Kestrel"));
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
 
-        public IConfiguration Configuration { get; }
+        var enableSwagger = Configuration.GetSection("Swagger").GetValue<bool>("enabled");
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        if (enableSwagger)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GestioneSagre.Web.SecurityApi", Version = "v1" });
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre v1");
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseCors();
+        app.UseEndpoints(endpoints =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GestioneSagre.Web.SecurityApi v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+            endpoints.MapControllers();
+        });
     }
 }
