6d88a943f23683c9c1fbc0d3f244943757a54a4d
Angelo Pirola <angelo9871@gmail.com>
2022-09-26 12:04:05 +0200

    Aggiunte nuove configurazioni Mapper (WIP)

---
+++ src/GestioneSagre.Mapper/CategoriaMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace GestioneSagre.Mapper;
+
+public class CategoriaMapper : IEntityTypeConfiguration<CategoriaEntity>
+{
+    public void Configure(EntityTypeBuilder<CategoriaEntity> entity)
+    {
+        entity.ToTable("Categoria");
+        entity.HasKey(categoria => categoria.Id);
+    }
+}
