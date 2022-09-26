d3d0ea726d160c5f41689a8b0cea7ff2c1ebde4e
Angelo Pirola <angelo@aepservice.it>
2022-09-26 12:17:17 +0200

    Aggiunte configurazioni Mapper

---
+++ src/GestioneSagre.Mapper/PrenotazioneDettaglioMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace GestioneSagre.Mapper;
+
+public class PrenotazioneDettaglioMapper : IEntityTypeConfiguration<PrenotazioneDettaglioEntity>
+{
+    public void Configure(EntityTypeBuilder<PrenotazioneDettaglioEntity> entity)
+    {
+        entity.ToTable("PrenotazioneDettaglio");
+        entity.HasKey(prenotazione => prenotazione.Id);
+    }
+}
---
+++ src/GestioneSagre.Mapper/PrenotazioneMapper.cs
@@ -0,0 +1,11 @@
+﻿namespace GestioneSagre.Mapper;
+
+public class PrenotazioneMapper : IEntityTypeConfiguration<PrenotazioneEntity>
+{
+    public void Configure(EntityTypeBuilder<PrenotazioneEntity> entity)
+    {
+        entity.ToTable("Prenotazione");
+        entity.HasKey(prenotazione => prenotazione.Id);
+        entity.Property(prenotazione => prenotazione.StatoPrenotazione).HasConversion<string>();
+    }
+}
